e82eb0dff645611633741d2684c6fe90d4ae405a
Brian Marinai <brianmarinai.bm@gmail.com>
2020-06-18 17:14:03 +0200

    esercizi terminati

--- jsnack-1/js/myscript.js
+++ jsnack-1/js/myscript.js
@@ -2,8 +2,8 @@ var numero = prompt ("inserisci un numero");
 
 var numero2 = prompt ("inserisci un secondo numero");
 
-if (numero > numero2) {
-  console.log(numero);
-} else if (numero2 > numero) {
+if (numero < numero2) {
   console.log(numero2);
+} else if (numero > numero2) {
+  console.log(numero);
 }
